1c5cc46f6c9f769962dce3d7cfe447c762604446
Timur Nagimov <nagimov_timur@bk.ru>
2022-07-19 14:10:14 +0300

    Make input/output for task

--- example.py
+++ example.py
@@ -9,6 +9,7 @@ from pprint import pprint
 
 from sys import argv
 script, path = argv
+out_file_name = path[path.rfind('/')-1:]
 
 
 class Cargo_group:
@@ -124,62 +125,9 @@ print(volume_left)
 #print(volume_used + volume_not_used)
 
 
-from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
-import matplotlib.pyplot as plt
-
-def cuboid_data(o, size=(1,1,1)):
-    # suppose axis direction: x: to left; y: to inside; z: to upper
-    # get the length, width, and height
-    l, w, h = size
-    x = [[o[0], o[0] + l, o[0] + l, o[0], o[0]],  
-         [o[0], o[0] + l, o[0] + l, o[0], o[0]],  
-         [o[0], o[0] + l, o[0] + l, o[0], o[0]],  
-         [o[0], o[0] + l, o[0] + l, o[0], o[0]]]  
-    y = [[-o[1], -o[1], -o[1] - w, -o[1] - w, -o[1]],  
-         [-o[1], -o[1], -o[1] - w, -o[1] - w, -o[1]],  
-         [-o[1], -o[1], -o[1], -o[1], -o[1]],          
-         [-o[1] - w, -o[1] - w, -o[1] - w, -o[1] - w, -o[1] - w]]   
-    z = [[o[2], o[2], o[2], o[2], o[2]],                       
-         [o[2] + h, o[2] + h, o[2] + h, o[2] + h, o[2] + h],   
-         [o[2], o[2], o[2] + h, o[2] + h, o[2]],               
-         [o[2], o[2], o[2] + h, o[2] + h, o[2]]]               
-    return np.array(x), np.array(y), np.array(z)
-
-def plotCubeAt(pos=(0,0,0), size=(1,1,1), ax=None,**kwargs):
-    # Plotting a cube element at position pos
-    if ax !=None:
-        X, Y, Z = cuboid_data( pos, size )
-        ax.plot_surface(X, Y, Z, rstride=1, cstride=1, **kwargs)
-        
-positions=[]
-sizes=[]
-colors=[]
-for key, item in enumerate(a.items):
-    (x, y, z) = item.position
-    (w, h, d) = item.dimension
-    position = (float(x), float(y), float(z))
-    size = (float(w), float(h), float(d))
-    color = (1 / (key+1), 1, 0.005 * key)
-    positions.append(position)
-    sizes.append(size)
-    colors.append(color)
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.set_aspect('equal')
-
-for p, s, c in zip(positions, sizes, colors):
-    plotCubeAt(pos=p, size=s, ax=ax, color=c)
-
-print("ALl items: ", len(cargos))
-print("In box items: ", count)
-plt.show()
-
 # TO JSON
 
-'''
+
 import nums_from_string
 
 output_info = {
@@ -263,7 +211,7 @@ output_info['unpacked'].append(
 
 pprint(output_info)
 
-with open('output/'+path, "w", encoding='utf-8') as f:
+with open('output/'+out_file_name, "w", encoding='utf-8') as f:
     json.dump(output_info, f, ensure_ascii=False, indent=4)
-'''
+
 # print("--- %s seconds ---" % (time.time() - start_time))
